89c4e3fdcd4093754812be5e5761140bdde0a6a5
Magzhan <em261777@gmail.com>
2025-09-16 11:26:34 +0300

    feat: added .gitignore

---
+++ go.mod
@@ -0,0 +1,5 @@
+module bubblesort
+
+go 1.24
+
+require github.com/go-echarts/go-echarts/v2 v2.6.2 // indirect
---
+++ main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/go-echarts/go-echarts/v2/charts"
+	"github.com/go-echarts/go-echarts/v2/opts"
+)
+
+func main() {
+	unsortedNumbers := GenerateArray(1000)
+
+	//We need a data structure for graph
+	results := make(map[string]float64)
+
+	// BubbleSort
+	numsForBubble := append([]int(nil), unsortedNumbers...)
+	startBubbleSortTimer := time.Now()
+	BubbleSort(numsForBubble)
+	elapsedBubble := time.Since(startBubbleSortTimer).Seconds() * 1000 // в мс
+	results["Bubble Sort O(n^2)"] = elapsedBubble
+	fmt.Printf("Bubble Sort took: %.4f ms\n", elapsedBubble)
+
+	// QuickSort
+	numsForQuick := append([]int(nil), unsortedNumbers...)
+	startQuickSortTimer := time.Now()
+	QuickSort(numsForQuick)
+	elapsedQuick := time.Since(startQuickSortTimer).Seconds() * 1000
+	results["Quick Sort O(n log n)"] = elapsedQuick
+	fmt.Printf("Quick Sort took: %.4f ms\n", elapsedQuick)
+
+	// InsertionSort
+	numsForIns := append([]int(nil), unsortedNumbers...)
+	startInsertionSortTimer := time.Now()
+	InsertionSort(numsForIns)
+	elapsedInsertion := time.Since(startInsertionSortTimer).Seconds() * 1000
+	results["Insertion Sort O(n^2)"] = elapsedInsertion
+	fmt.Printf("Insertion Sort took: %.4f ms\n", elapsedInsertion)
+
+	BuildChart(results)
+}
+
+func GenerateArray(size int) []int {
+	numbers := make([]int, size)
+	for i := 0; i < size; i++ {
+		numbers[i] = rand.Intn(size)
+	}
+
+	return numbers
+}
+
+func QuickSort(nums []int) []int {
+	if len(nums) < 2 {
+		return nums
+	} else {
+		pivot := nums[0]
+		less := []int{}
+		greater := []int{}
+
+		for _, num := range nums[1:] {
+			if num <= pivot {
+				less = append(less, num)
+			} else {
+				greater = append(greater, num)
+			}
+		}
+
+		return append(append(QuickSort(less), pivot), QuickSort(greater)...)
+	}
+}
+
+func BubbleSort(nums []int) []int {
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i] > nums[j] {
+				nums[i], nums[j] = nums[j], nums[i]
+			}
+		}
+	}
+
+	return nums
+}
+
+func InsertionSort(nums []int) []int {
+	for i := 0; i < len(nums); i++ {
+		key := nums[i]
+		j := i - 1
+		for j >= 0 && key < nums[j] {
+			nums[j+1] = nums[j]
+			j = j - 1
+		}
+		nums[j+1] = key
+	}
+
+	return nums
+}
+
+func BuildChart(results map[string]float64) {
+	bar := charts.NewBar()
+	bar.SetGlobalOptions(
+		charts.WithTitleOpts(opts.Title{
+			Title:    "Sorting Algorithm Performance",
+			Subtitle: "ms",
+		}),
+	)
+
+	names := []string{}
+	values := []opts.BarData{}
+	for name, val := range results {
+		names = append(names, name)
+		values = append(values, opts.BarData{Value: val})
+	}
+
+	bar.SetXAxis(names).AddSeries("Time (ms)", values)
+
+	f, _ := os.Create("sorting_results.html")
+	defer f.Close()
+	bar.Render(f)
+}
